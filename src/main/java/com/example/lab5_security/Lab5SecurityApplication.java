1ae0e1cff53c0e8f9f515dbb206a00e23c5089d4
user <gainishka.tleuova@gmail.com>
2024-12-01 23:17:02 +0600

    first commit

---
+++ src/main/java/com/example/lab5_security/Lab5SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab5_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5SecurityApplication.class, args);
+    }
+
+}
